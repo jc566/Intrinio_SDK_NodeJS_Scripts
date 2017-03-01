b1eb0718c5093eceef32afa524c9b292b2f94510
Joseph Choi <jc566@njit.edu>
2017-03-01 06:18:20 -0500

    Send/Recieve info from/to b'browser'

--- index.js
+++ index.js
@@ -3,77 +3,53 @@ var username = 'b53ca7da9d4a36ec6c2880700d200928'
 var password = '4f7569d13ad10073691481e2a2667e3f'
 var intrinio = require('intrinio-client')(username, password)
 
-// express
 var express = require('express')
 var app = express()
 
+app.get('/ping', function(data, response) {
+	console.log('recieved ping, sending pong')
+	response.json('pong')
+})
+/************************************************
+Get stock information by providing ticker symbol*
+						*
+Example: /stocks?sym=aapl 			*
+Example for Keys: 				*
+/stocks?keys=ticker				*
+/stocks?keys=legal_name				*
+/stocks?keys=short_description			*
+/stocks?keys=ticker,short_description		*
+************************************************/
+app.get('/stocks', function(data, response) {
 
-app.get('/ping', function (data, response) {
-  console.log('received ping, sending pong')
-  response.json({ text: 'pong' })
+	//this is the variable that holds user input
+	var sym = data.query.sym.toUpperCase()	//ensure that the query is uppercase
+
+	//this is for collection of keys
+	var keys = data.query.keys 
+	//create empty object
+	var temp = {} 
+
+	
+	intrinio.ticker(sym) //make an api call with sym
+	.on('complete', function(tickerData,tickerResponse){
+		if(tickerData) {//if there is ticker data then ...
+				
+
+
+		console.log(tickerData.ticker)
+		console.log(tickerData.legal_name)
+		console.log(tickerData.short_description)
+	
+		temp = tickerData.short_description
+		response.json(temp) //send response to client (this can be "browser" or another file like php file)
+		}
+	})
 })
 
-/**
- * Get stock information by providing ticker symbol (required)
- * example: /stocks?sym=AAPL
- *
- * Provide a CSV of keys to fetch (optional, default: ticker,legal_name,stock_echange)
- * Use either of the following methods:
- * 1. example: /stocks?keys=ticker,legal_name,stock_exchange
- */
-app.get('/stocks', function (data, response) {
-  // TODO safety checks:
-  // make sure sym was passed
-  // make sure sym is valid
-  // no numbers
-
-  // TODO safety checks:
-  // check the keys
-
-  // user input
-  var sym = data.query.sym.toUpperCase()
-
-  var defaultKeys = [
-    'ticker',
-    'legal_name',
-    'stock_exchange'
-  ]
-  var keys = data.query.keys || defaultKeys
-
-  var keysAsArray
-
-  if (typeof keys === 'string') {
-    // turn the csv string into an array
-    keysAsArray = keys.split(',')
-  } else if (typeof keys === 'array') {
-    // leave it alone
-    keysAsArray = keys
-  } else {
-    // idk what "keys" is
-    // TODO throw an error
-  }
-
-  // declare the info object
-  var info = {}
-
-  intrinio.ticker(sym) // make an api call (async)
-  .on('complete', function (d, r) {
-    // ... this might take a while
-    if (d) {
-      for (var i = 0; i < keysAsArray.length; i++) {
-        var key = keysAsArray[i]
-        info[key] = d[key] // IMPORTANT! use []-notation, not dot-notation
-      }
-
-      // send response to "browser" (client)
-      response.json(info) // 200
-    } else {
-      console.log('invalid symbol: ' + sym)
-      response.json({ 'error': 'not_found' }) // TODO set status code to 404
-    }
-  })
+//the '9090' can be replaced with the proper server we are using
+app.listen(9090, function(){
+	console.log('Im listening to server local host 9090')
 })
 
-app.listen(9090, function () {
-  console.log('listening on port 9090!')
-})
+
